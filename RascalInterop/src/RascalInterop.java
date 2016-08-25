770fd3466b30a469cc8f0805db1d42ba98d18e65
Klemens Schindler <klemensschindler@gmail.com>
2016-08-25 12:22:41 +0200

    Fixed Rascal interop demo with fix provided by Tijs van der Storm.

--- RascalInterop/src/RascalInterop.java
+++ RascalInterop/src/RascalInterop.java
@@ -8,29 +8,30 @@ import org.rascalmpl.interpreter.load.URIContributor;
 import org.rascalmpl.uri.URIUtil;
 
 public class RascalInterop {
-    public static void main(String[] args) throws URISyntaxException {
-        JavaToRascal j2r = new JavaToRascal(new PrintWriter(System.out), new PrintWriter(System.err));
+	public static void main(String[] args) throws URISyntaxException {
+		JavaToRascal j2r = new JavaToRascal(new PrintWriter(System.out), new PrintWriter(System.err));
 
-        IRascalSearchPathContributor modulePath = new URIContributor(URIUtil.createFileLocation("C:/repo/github/rascal-experiment/RascalInterop/src"));
-        j2r.getEvaluator().addRascalSearchPathContributor(modulePath);
-        
-        try {
-        	System.out.println("Eval: import MyTest;");
-            j2r.voidValue("import MyTest;"); // Exception
-            j2r.eval("hello();");
-        } catch (Exception e) {
-            System.out.println("EXCEPTION: " + e.getClass() + " " + e.getMessage());
-        	e.printStackTrace();
-        }
+		IRascalSearchPathContributor modulePath = new URIContributor(
+				URIUtil.correctLocation("file", "", "/repo/github/rascal-experiment/RascalInterop/src"));
+		j2r.getEvaluator().addRascalSearchPathContributor(modulePath);
+		j2r.getEvaluator().addRascalSearchPathContributor(StandardLibraryContributor.getInstance());
 
-        j2r.getEvaluator().addRascalSearchPathContributor(StandardLibraryContributor.getInstance());
-        try {
-            j2r.eval("import IO;"); // null pointer exception
-        } catch (Exception e) {
-            System.out.println("EXCEPTION: " + e.getClass() + " " + e.getMessage());
-        	e.printStackTrace();
-        }
-                
-    	//j2r.eval("writeFile(|cwd:///hello.txt|, \"Helloooo!\");");
-    }
+		try {
+			System.out.println("Eval: import MyTest;");
+			j2r.voidValue("import MyTest;", "stdin:///"); // Exception
+			j2r.voidValue("hello();", "stdin:///");
+		} catch (Exception e) {
+			System.out.println("EXCEPTION: " + e.getClass() + " " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		try {
+			j2r.voidValue("import IO;", "stdin:///"); // null pointer exception
+		} catch (Exception e) {
+			System.out.println("EXCEPTION: " + e.getClass() + " " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		// j2r.eval("writeFile(|cwd:///hello.txt|, \"Helloooo!\");");
+	}
 }
